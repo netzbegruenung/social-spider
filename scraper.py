808b2a664129609c5660b30bec922af185ad81e9
Lukas Mehl <lm041@hdm-stuttgart.de>
2018-07-21 13:07:00 +0200

    refactor both scrapers to one file

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 
-def scrape(username):
+def scrapeInstagramData(username):
     url = "https://www.instagram.com/" + username
     r = requests.get(url)
     
@@ -22,6 +22,19 @@ def scrape(username):
     else:
         print("No data found", file=sys.stderr)
         
+def scrapeFacebookLikes(username):
+    url = "https://www.facebook.com/" + username
+    r = requests.get(url)
+    
+    s = str(r.content)
+    
+    pattern = "Gef&#xe4;llt ([\d\.]+) Mal"
+    result = re.search(pattern, s)
+    if result:
+        return int(result[1].replace(".", ""))
+    else:
+        print("No data found", file=sys.stderr)
 
 if __name__ == '__main__':
-    pprint(scrape("die_gruenen"))
+    pprint(scrapeInstagramData("die_gruenen"))
+    print(scrapeFacebookLikes("B90DieGruenen"))
