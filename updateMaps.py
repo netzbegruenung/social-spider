e074a773de397d66eee2e69fad6ed69f28eb5550
Lukas Mehl <lukas.mehl@gmail.com>
2019-09-10 16:31:34 +0200

    use logarithmic scale for KV, move social data to toplevel

--- updateMaps.py
+++ updateMaps.py
@@ -1,4 +1,5 @@
 import json
+import math
 
 def updateMaps(level="lv"):
     data = {}
@@ -30,18 +31,29 @@ def updateMaps(level="lv"):
             social_data["tw_count"] = tw_count
             social_data["in_name"] = in_name
             social_data["in_count"] = in_count
-        feature["properties"]["social-data"] = social_data
+        feature["properties"].update(social_data)
 
     print(fb_max, tw_max, in_max)
     plattform = "fb"
     for plattform, maxval, col in zip(["fb", "tw", "in"], [fb_max, tw_max, in_max], ["#3664a2", "#55acee", "#d02e92"]):
         for feature in map_data["features"]:
-            count = feature["properties"]["social-data"].get(plattform + "_count", 0)
-            feature["properties"]["fill-opacity"] = count/maxval
+            count = feature["properties"].get(plattform + "_count", 0)
+            if level=="lv":
+                # linear for LV
+                feature["properties"]["fill-opacity"] = count/maxval
+            elif level == "kv":
+                # logarithmic for KV
+                feature["properties"]["fill-opacity"] = getLogValue(count, maxval)
             feature["properties"]["fill"] = col
             feature["properties"]["stroke-width"] = 0
         with open("maps/" + level + "_" + plattform + ".geojson", "w") as f:
             json.dump(map_data, f, indent=4)
+
+def getLogValue(count, maxval):
+    multiplier = 0.1
+    if count * multiplier < 1:
+        return 0
+    return math.log(count*multiplier) / math.log(maxval*multiplier)
 
 if __name__ == "__main__":
     updateMaps(level="kv")
